508b58a05f405e9573b2d1ff54a03b4e2db21220
vishalr316 <vishalshawn@gmail.com>
2024-05-28 23:08:55 +0530

    Solution for leetcode problem - Plus_one

---
+++ leetcode-plus_one.py
@@ -0,0 +1,11 @@
+# Solution for leetcode problem - https://leetcode.com/problems/plus-one/description/?envType=study-plan-v2&envId=programming-skills
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in reversed(range(len(digits))):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        return [1] + digits
