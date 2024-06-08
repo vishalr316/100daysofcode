2b35ec171d9606ef1669df3c22a1d9fc8ff2ee2c
vishalr316 <36631605+vishalr316@users.noreply.github.com>
2024-06-08 20:30:24 +0530

    Add files via upload
    Day 13 updates

---
+++ Leetcode-jewels-stones.py
@@ -0,0 +1,9 @@
+# Solution for - https://leetcode.com/problems/jewels-and-stones/
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        for i in stones:
+            if i in jewels:
+                count += 1
+        return count
